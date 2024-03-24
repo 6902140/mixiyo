4915e5a4995fd34b6f05c4c1459cbf031d233f3f
Jy Feng <fengjy926@gmail.com>
2024-03-24 09:12:50 +0800

    fix loop & animation bugs in Start Scene

--- Assets/@Project/Scripts/StartScene/GameManager.cs
+++ Assets/@Project/Scripts/StartScene/GameManager.cs
@@ -7,9 +7,12 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Animator blueAnimator = null;
+    public Animator redAnimator = null;
     void Start()
     {
-        
+        blueAnimator.Play("Walk");
+        redAnimator.Play("Walk");
     }
 
     // Update is called once per frame
